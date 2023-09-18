3ed653be1b4ea8c50d854ca2582e0c7d318439e6
mabadia <angel.beyou95@gmail.com>
2023-09-17 21:09:30 -0700

    complete

---
+++ src/components/StoreListings.js
@@ -0,0 +1,42 @@
+import { Container } from "react-bootstrap";
+
+export default function StoreListings() {
+    return (
+        <div style={{ margin: "auto" }}>
+            <h2>Browse stores in Houston</h2>
+            <Container>
+                <Row style={{ borderBottom: "1px solid #E8E9EB", display: "flex", justifyContent: "space-evenly" }} >
+                    <Row>
+                        <Col xs=".5" >
+                            <Image src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/12/2774c343-1319-4720-a54f-0c46e93f956c.png" roundedCircle />
+                        </Col>
+                        <Col>
+                            <h5 style={{ fontWeight: "bold" }}>ALDI</h5>
+                            <p>Delivery * Pickup</p>
+                        </Col>
+                    </Row>
+                    <Col sm="1"></Col>
+                    <Row >
+                        <Col xs=".5">
+                            <Image src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/279/0d43b5d2-9d07-4a30-a0ee-0e16d5922411.png" roundedCircle />
+                        </Col>
+                        <Col>
+                            <h5 style={{ fontWeight: "bold" }}>Sprouts Farmers Market</h5>
+                            <p>Delivery * Pickup</p>
+                        </Col>
+                    </Row>
+                    <Col sm="1"></Col>
+                    <Row >
+                        <Col xs=".5" >
+                            <Image src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/45/d10d0f4b-1fca-4b84-bc19-bbf492c040da.png" roundedCircle />
+                        </Col>
+                        <Col md="6">
+                            <h5 style={{ fontWeight: "bold" }}>Kroger</h5>
+                            <p>Delivery</p>
+                        </Col>
+                    </Row>
+                </Row>
+            </Container>
+        </div>
+    )
+}
